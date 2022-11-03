b58a80d8002c40aff215a899081b4bc40b62d6f8
Christiaan de Kock <christiaandk96@gmail.com>
2022-11-03 18:05:18 +0200

    removal of react template contents, added framer motion, tailwind and smooth scroll, full tailwind setup

---
+++ src/hooks/useMediaQuery.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+// determines if you match particular breakpoint of media screensize
+// Alerts if current screen size is for example higher/lower than what max/min width was set to
+
+const useMediaQuery = (query) => {
+    const [matches, setMatches] = useState(false);
+
+    useEffect(() => {
+        const media = window.matchMedia(query);
+        if (media.matches !== matches){
+            setMatches(media.matches);
+        }
+
+        const listener = () => setMatches(media.matches);
+        window.addEventListener("resize", listener);
+        return () => window.removeEventListener("resize", listener);
+    }, [matches, query]);
+
+    return matches;
+}
+
+export default useMediaQuery;
