7c51f7f9e655dc4cad9e7030d5240b8a782922f3
Christiaan de Kock <christiaandk96@gmail.com>
2022-11-03 21:22:04 +0200

    Added navbar and side dot buttons for page select

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 
-import './App.css';
+import Navbar from "./scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
+  // set state of selectedpage default to home and check screensize
+
+  const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isAboveMediumScreens = useMediaQuery("min-width: 1060px");
+
+  useEffect(() => {
+
+    // checking window which gives position of y and what to di at values of 0 or not 0
+    const handleScroll = () => {
+        if (window.scrollY === 0) setIsTopOfPage(true);
+        if (window.scrollY !== 0) setIsTopOfPage(false);
+      }
+      window.addEventListener("scroll", handleScroll);
+      return() => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="app bg-deep-blue">
+      <Navbar 
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage} 
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+      <DotGroup 
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+      />
+      )}
+      </div>
     </div>
   );
 }
